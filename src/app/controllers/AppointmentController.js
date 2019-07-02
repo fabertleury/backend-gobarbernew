736949df98936a7be6d04551ee3fb18319b0f72f
Fabert Leury <fabert_@hotmail.com>
2019-07-02 17:04:07 -0300

    agendamento de usuarios

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -18,14 +19,44 @@ class AppointmentController {
     /**
      * CHECK LIST PROVIDER_ID IS A PROVIDER
      */
-    const isProvider = await User.findOne({
+    const checkIsProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
 
-    if (!isProvider) {
+    if (!checkIsProvider) {
       return res
         .status(401)
         .json({ error: 'Você não pode criar agendamentos sem providers' });
+    }
+
+    /**
+     * CHECANDO SE AGENDAMENTO ESTA COM DATAS RETROATIVAS.
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Não é permitido criar agendamentos com datas retroativas.',
+      });
+    }
+
+    /**
+     * CHECANDO SE AGENDAMENTO ESTA HABILITADO PARA A DATA.
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Agendamento não está habilitado para esta data.' });
     }
 
     const appointment = await Appointment.create({
