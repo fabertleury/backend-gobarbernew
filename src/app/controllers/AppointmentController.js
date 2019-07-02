d6c1ab7aca6edb1bc27d9feb969bb3a386cd2d66
Fabert Leury <fabert_@hotmail.com>
2019-07-02 16:45:28 -0300

    appointments

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,41 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * CHECK LIST PROVIDER_ID IS A PROVIDER
+     */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Você não pode criar agendamentos sem providers' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
