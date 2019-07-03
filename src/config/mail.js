f1d9893919f3070754cfa9d25627e9f8d1edbb92
Fabert Leury <fabert_@hotmail.com>
2019-07-03 10:46:39 -0300

    NodeMailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd964d8462d9632',
+    pass: '87861f3780f84f',
+  },
+  default: {
+    from: 'Equipe Birds <noreply@birdsdigital.com.br>',
+  },
+};
